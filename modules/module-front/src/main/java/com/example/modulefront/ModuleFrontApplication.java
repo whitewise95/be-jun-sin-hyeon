4a9f0a50597aa4c60b5af9c13a98b2c62dff226d
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-02-18 21:38:57 +0900

    컴포넌트 스캔 추가

--- modules/module-front/src/main/java/com/example/modulefront/ModuleFrontApplication.java
+++ modules/module-front/src/main/java/com/example/modulefront/ModuleFrontApplication.java
@@ -2,8 +2,14 @@ package com.example.modulefront;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example"})
+@EntityScan(basePackages = {"com.example"})
+@EnableJpaRepositories(basePackages = {"com.example"})
 public class ModuleFrontApplication {
 
 	public static void main(String[] args) {
